6c47ae3c4080dba088f5101b55b7b99624deb3da
Jan Pęski <74868334+thepeski@users.noreply.github.com>
2022-12-15 16:31:49 +0100

    finished the egg timer exercise

--- Swift/Exercises/EggTimer-iOS13/EggTimer/ViewController.swift
+++ Swift/Exercises/EggTimer-iOS13/EggTimer/ViewController.swift
@@ -9,23 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let eggTimes = ["Soft": 5 * 120, "Medium": 7 * 120, "Hard": 12 * 120]
+    var totalTime = 0
+    var timePassed = 0
     
-    let soft = 5 * 60
-    let medium = 7 * 60
-    let hard = 12 * 60
+    var timer = Timer()
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var timerLabel: UILabel!
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle!
         
-        switch hardness {
-        case "Soft":
-            print(soft)
-        case "Medium":
-            print(medium)
-        case "Hard":
-            print(hard)
-        default:
-            print("Error")
+        timer.invalidate() // reset timer
+        progressBar.progress = 0.0
+        timePassed = 0
+        timerLabel.text = hardness
+        
+        
+        totalTime = eggTimes[hardness]!
+        
+        // calls the selector function every second
+        timer = Timer.scheduledTimer(timeInterval: 1 / 120, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer() { // specify we're using objective C selectors
+        if timePassed < totalTime {
+            timePassed += 1
+            let percentageProcess = Float(timePassed) / Float(totalTime)
+            
+            progressBar.progress = percentageProcess
+        } else {
+            timer.invalidate() // stop timer
+            timerLabel.text = "Done!"
         }
     }
     
