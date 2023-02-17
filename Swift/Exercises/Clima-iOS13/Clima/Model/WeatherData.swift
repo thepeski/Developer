1a79bc6f913705446b405499235b2f81af310b05
Jan Pęski <74868334+thepeski@users.noreply.github.com>
2023-02-17 14:19:26 +0100

    updated files

---
+++ Swift/Exercises/Clima-iOS13/Clima/Model/WeatherData.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Jan Pęski on 08/02/2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Swift/Exercises/Clima-iOS13/Clima/Model/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Jan Pęski on 09/02/2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
